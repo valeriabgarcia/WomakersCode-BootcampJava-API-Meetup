55603119b61085d22d7e7de35b19fc2e9d532a39
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-04-08 14:12:34 -0300

    meetup project created

---
+++ src/main/java/com/bootcamp/microservicemeetup/MeetupSchedulingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.microservicemeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupSchedulingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupSchedulingServiceApplication.class, args);
+	}
+
+}
